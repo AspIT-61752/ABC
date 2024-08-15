8f485d8d73e0c65759a0cd28a6575dc2fde35571
AspIT-61752 <61752@edu.campusvejle.dk>
2024-08-15 13:32:45 +0200

    Basic Entities are done

---
+++ src/ABC.Entity/Savings.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ABC.Entity
+{
+    public class Saving
+    {
+        private int savingId;
+        private string savingName;
+        private User user;
+        private int savingsGoal;
+        private int savingsBalance;
+
+        [JsonConstructor]
+        public Saving() { }
+
+        public Saving(int savingId, string savingName, User user, int savingsGoal, int savingsBalance)
+        {
+            SavingId = savingId;
+            SavingName = savingName;
+            User = user;
+            SavingsGoal = savingsGoal;
+            SavingsBalance = savingsBalance;
+        }
+
+        public int SavingId { get => savingId; set => savingId = value; }
+        public string SavingName { get => savingName; set => savingName = value; }
+        public User User { get => user; set => user = value; }
+        public int SavingsGoal { get => savingsGoal; set => savingsGoal = value; }
+        public int SavingsBalance { get => savingsBalance; set => savingsBalance = value; }
+    }
+}
---
+++ src/ABC.Entity/Users.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ABC.Entity
+{
+    public class User
+    {
+        private int userId;
+        private string username;
+        private string password;
+        private string email;
+
+        [JsonConstructor]
+        public User() { }
+
+        public User(int id, string username, string password, string email)
+        {
+            UserId = id;
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+
+        public int UserId { get => userId; set => userId = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
